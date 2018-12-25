5136faaadf0fd7af6d5ec09fbe96e4087746bb68
mqvista <mqvista@gmail.com>
2018-12-25 17:05:47 +0800

    Test

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     //LogInfo logInfo;
     //qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
 
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    //QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
 
